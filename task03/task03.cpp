d93dfd60573b941782f7a09c6f06a1305bb0281c
lightman1998 <kall-lightman@yandex.ru>
2017-11-20 13:02:31 +0300

    Create task03.cpp
    Put task03 code

---
+++ task03/task03.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node * next;
+};
+
+class List
+{
+    Node *Head;
+public:
+    List() { Head=NULL; }
+    ~List();
+    void Add(int x);
+    void Show();
+    Node * reverse( Node * head);
+};
+
+List::~List() {
+    while(Head!=NULL) {
+        Node *temp = Head->next;
+        delete Head;
+        Head = temp;
+    }
+}
+
+void List:: Add(int x) {
+    Node *temp = new Node;
+    temp-> data = x;
+    temp-> next = Head;
+    Head = temp;
+}
+
+void List::Show()
+{
+    Node *temp = Head;
+    while(temp!= NULL) {
+        cout << temp->data << "";
+        temp = temp->next;
+    }
+}
+Node * List:: reverse( Node * head) {
+    Node *reversed_list = NULL;
+    for (Node *node = head, *next_nod; node !=NULL; node = next_nod) {
+        next_nod = node->next;
+        node->next = reversed_list;
+        reversed_list = node;
+    }
+    return reversed_list;
+}
+
+int main(){
+    return 0;
+}
